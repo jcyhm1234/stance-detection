cbe0e3a572d04d9327b57081e68595c5c778fe0b
Ashwini Venkatesh <asvenk@wireless-10-147-134-41.public.utexas.edu>
2016-05-07 16:29:43 -0500

    Adding contain feature

--- src/featureExtraction.py
+++ src/featureExtraction.py
@@ -2,13 +2,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 from dataPreprocess import DataPreprocess
 from lexicon import SubjLexicon, LiuLexicon
 from CMUTweetTagger import runtagger_parse
+from nltk.tokenize import TweetTokenizer
 
 class FeatureExtract:
 	def __init__(self, data):
 		self.data = data
 		self.counts = []
 		self.count_vect = CountVectorizer(decode_error='ignore')
+		self.corpus = []
+		self.tknzr = TweetTokenizer()
+		self.document_corpus()
+		self.liu = None
+		self.subj = None
 
+
+	#Unused 
 	def vectorizeFitTransform(self):
 		#TODO: Resolve decoding error ?
 		self.counts = self.count_vect.fit_transform(self.data)
@@ -17,6 +25,7 @@ class FeatureExtract:
 		# print trainCounts.shape
 		# print trainCounts[0]
 	
+	#Unused 
 	def vectorizeTransform(self, testData):
 		return self.count_vect.transform(testData)
 
@@ -29,11 +38,48 @@ class FeatureExtract:
 
 	def getPOSTags(self):
 		pos = runtagger_parse(self.data)
+		return pos
 		#[[(tuple-word,tag,confidence),('word','V',0.99)][]]
 		#change structure appropriateyl
+
+	def document_corpus(self):
+		for tweet in self.data:
+			self.corpus.extend(self.tknzr.tokenize(tweet))
+		self.corpus = set(self.corpus)
+
+
+	def document_features(self, document): 
+		#Gets the document feature for a single tweet
+		word_features = self.corpus
+		document_words = set(self.tknzr.tokenize(document))
+		features = {}
+		for word in word_features:
+			features['contains({})'.format(word)] = (word in document_words)
+		return features
+
+	def get_BOW_feature(self):
+		features = []
+		for tweet in self.data:
+			features.append(self.document_features(tweet))
+		return features
+
+	def get_all_features(self):
+		pos = self.getPOSTags()
+		self.getLexiconFeatures()
+		liu = self.liu
+		subj = self.subj
+		bow = self.get_BOW_feature()
+		print 'Length of pos features ', len(pos[0])
+		print 'Length of liu features ', len(liu[0])
+		print 'Length of subj features ', len(subj[0])
+		print 'Length of bow features ', len(bow[0])
+
+
 
 if __name__ == '__main__':
 	dp = DataPreprocess('../data/train.csv','../data/test.csv')
 	fe = FeatureExtract(dp.trainTweets)
-	fe.vectorizeFitTransform()
-	fe.getPOSTags()
+	print 'Extracting features for data'
+	fe.get_all_features()
+	# fe.vectorizeFitTransform()
+	# fe.getPOSTags()
